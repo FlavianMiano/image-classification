b4a4ae73bc015c629138e301a5d7c93de1811875
FlavianMiano <48262134+FlavianMiano@users.noreply.github.com>
2023-03-15 20:11:24 +0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,52 @@
+import streamlit as st
+import tensorflow as tf
+import streamlit as st
+import cv2
+from PIL import Image, ImageOps
+import numpy as np
+
+
+def load_model():
+  model=tf.keras.models.load_model('my_model')
+  return model
+with st.spinner('Model is being loaded..'):
+  model=load_model()
+
+st.write("""
+         # Pnemonia Prediction
+         """
+         )
+
+file = st.file_uploader("Please upload an brain scan file", type=["jpeg", "png"])
+
+st.set_option('deprecation.showfileUploaderEncoding', False)
+def import_and_predict(image_data, model):
+    
+        size = (150,150)    
+        image = ImageOps.fit(image_data, size, Image.ANTIALIAS)
+        image = np.asarray(image)
+        img = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        #img_resize = (cv2.resize(img, dsize=(75, 75),    interpolation=cv2.INTER_CUBIC))/255.
+        
+        img_reshape = img[np.newaxis,...]
+    
+        prediction = model.predict(img_reshape)
+        
+        return prediction
+
+if file is None:
+    st.text("Please upload an image file")
+else:
+    image = Image.open(file)
+    st.image(image, use_column_width=True)
+    predictions = import_and_predict(image, model)
+    score = tf.nn.softmax(predictions[0])
+    class_names=['Normal','Pnemonia',]
+    st.write('0 = Normal, 1 = Pnemonia')
+    st.write(predictions)
+    st.write(score)
+    #print("This image most likely belongs to {} with a {:.2f} percent confidence."
+    #.format(class_names[np.argmax(predictions)], 100 * np.max(score)))
+
+    #string='Patient most likely to have: '+class_names[np.argmax(predictions)]
+    #st.success(string)
